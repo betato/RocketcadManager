e519b13ec13a8c6b7f2bfbbad807a3053d3618b6
betato <battlestargatticus@gmail.com>
2020-09-16 16:26:33 -0400

    Add FolderNode tree to store file view expansion

--- RocketcadManagerLib/Config.cs
+++ RocketcadManagerLib/Config.cs
@@ -3,25 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RocketcadManagerLib
 {
     public class Config
     {
+        private class FolderNode
+        {
+            public FolderNode sibling;
+            public FolderNode child;
+
+            public string Name { get; private set; }
+            public bool Expanded { get; private set; }
+
+            public FolderNode(string name, bool expanded)
+            {
+                Name = name;
+                Expanded = expanded;
+            }
+
+            public void Clear()
+            {
+                sibling = null;
+                child = null;
+            }
+        }
+
+        public List<string> CadDirectories { get; set; }
+        private FolderNode folderViewExpansion;
+
         public Config()
         {
             CadDirectories = new List<string>();
         }
 
-        public Config(Config config) : this()
+        public void SaveTreeViewExpansion(TreeNodeCollection treeNodes)
         {
-            // Copy constructor
-            foreach (string cadDirectory in config.CadDirectories)
-            {
-                CadDirectories.Add(cadDirectory);
-            }
+
+            folderViewExpansion = SaveTreeViewNodes(treeNodes);
         }
 
-        public List<string> CadDirectories { get; set; }
+        private FolderNode SaveTreeViewNodes(TreeNodeCollection treeNodes)
+        {
+            FolderNode firstSibling = null;
+            FolderNode currentSibling = null;
+            foreach (TreeNode treeNode in treeNodes)
+            {
+                TreeNodeCollection subNodes = treeNode.Nodes;
+                if (subNodes.Count > 0) // Node has children and is expandable
+                {
+                    if (firstSibling == null)
+                    {
+                        firstSibling = new FolderNode(treeNode.Name, treeNode.IsExpanded);
+                        currentSibling = firstSibling;
+                    }
+                    else
+                    {
+                        currentSibling.sibling = new FolderNode(treeNode.Name, treeNode.IsExpanded);
+                        currentSibling = currentSibling.sibling;
+                        currentSibling.child = SaveTreeViewNodes(subNodes);
+                    }
+                }
+            }
+            return firstSibling;
+        }
     }
 }
